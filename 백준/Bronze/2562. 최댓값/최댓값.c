5d3fddc98debc51d876fea983acacd67f9970e56
uzichoi <cjiyou1090@gmail.com>
2025-01-25 18:04:26 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int input;
+	int max = 0;
+	int pos = 0;
+
+	for (int i = 1; i < 10; ++i) {
+		scanf("%d", &input);
+		if (input > max) {
+			max = input;
+			pos = i;
+		}
+	}
+
+	printf("%d\n%d\n", max, pos);
+	return 0;
+}
